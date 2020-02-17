1e6e0aa9ffd8e7d4b069ae843b51cf157758f349
Guillermo Palma <gapalmasolano@gmail.com>
2020-02-17 09:40:51 -0700

    Add  Project Base | Infrastructure

---
+++ Source/POS/App.Infrastructure/Data/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace App.Infrastructure.Data
+{
+    public class DataContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                /*
+                 * To protect potentially sensitive information in your connection string, you should move it out of source code. 
+                 * See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                 */
+                //optionsBuilder.UseMySql("server=localhost;user id=root;password=;database=app_pos;");
+            }
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
