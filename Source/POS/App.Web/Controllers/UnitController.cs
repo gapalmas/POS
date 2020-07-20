c8e8099349fa89f1d2f450c2acc7c200e907148d
Guillermo Alberto Palma Solano <gapalmas@outlook.com>
2020-07-20 08:38:57 -0800

    Add Controller & Views on Company - Unit - Warehouse but created on Entity Wizard & Adjusting  MainLayout

---
+++ Source/POS/App.Web/Controllers/UnitController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using App.Core.Entities;
+using App.Infrastructure.Data;
+
+namespace App.Web.Controllers
+{
+    public class UnitController : Controller
+    {
+        private readonly DataContext _context;
+
+        public UnitController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Unit
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Unit.ToListAsync());
+        }
+
+        // GET: Unit/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var unit = await _context.Unit
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (unit == null)
+            {
+                return NotFound();
+            }
+
+            return View(unit);
+        }
+
+        // GET: Unit/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Unit/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Description,Measure,Id,Status,Date,DateUpdate")] Unit unit)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(unit);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(unit);
+        }
+
+        // GET: Unit/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var unit = await _context.Unit.FindAsync(id);
+            if (unit == null)
+            {
+                return NotFound();
+            }
+            return View(unit);
+        }
+
+        // POST: Unit/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Description,Measure,Id,Status,Date,DateUpdate")] Unit unit)
+        {
+            if (id != unit.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(unit);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!UnitExists(unit.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(unit);
+        }
+
+        // GET: Unit/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var unit = await _context.Unit
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (unit == null)
+            {
+                return NotFound();
+            }
+
+            return View(unit);
+        }
+
+        // POST: Unit/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var unit = await _context.Unit.FindAsync(id);
+            _context.Unit.Remove(unit);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool UnitExists(int id)
+        {
+            return _context.Unit.Any(e => e.Id == id);
+        }
+    }
+}
