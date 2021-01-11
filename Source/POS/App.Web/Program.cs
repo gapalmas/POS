435c47506ebc663808735e455847a0580e940d1c
Guillermo Alberto Palma Solano <gapalmas@outlook.com>
2021-01-11 10:11:05 -0700

    Update Target Framrework to .Net Core 3.1 & Test Migration OK

--- Source/POS/App.Web/Program.cs
+++ Source/POS/App.Web/Program.cs
@@ -1,45 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using App.Web.Data;
-using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace App.Web
 {
     public class Program
     {
-        //Reference: https://docs.microsoft.com/en-us/aspnet/core/data/ef-mvc/intro?view=aspnetcore-2.2
         public static void Main(string[] args)
         {
-            var host = CreateWebHostBuilder(args).Build();
-
-            using (var scope = host.Services.CreateScope())
-            {
-                var services = scope.ServiceProvider; /* creamos el alcance del servicio*/
-                try
-                {
-                    var seeder = services.GetRequiredService<Seeder>(); /*instanciamos nuestro servicio, en este caso al Seeder*/
-
-                    seeder.SeederAsync().Wait(); /*Mandamos a llamar el Seeder*/
-                }
-                catch (Exception ex) /*Capturamos el error en caso que exista*/
-                {
-                    var logger = services.GetRequiredService<ILogger<Program>>();
-                    logger.LogError(ex, "An error occurred while seeding the database.");
-                }
-            }
-
-            host.Run(); /*Ejecutamos el host*/
+            CreateHostBuilder(args).Build().Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
--- Source/POS/App.Web/Startup.cs
+++ Source/POS/App.Web/Startup.cs
@@ -1,22 +1,15 @@
-﻿using Microsoft.AspNetCore.Builder;
+using App.Infrastructure.Data;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
-using App.Core.Entities;
-using App.Infrastructure.Data;
-using App.Web.Helpers;
-using App.Web.Data;
-using AutoMapper;
+using Microsoft.Extensions.Hosting;
 using System;
-using App.Core.Interfaces;
-using App.Core.UseCases;
-using App.Web.Features.Alerts;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace App.Web
 {
@@ -32,112 +25,16 @@ namespace App.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
-            // //Auto Mapper Configurations
-            //var mappingConfig = new MapperConfiguration(mc =>
-            //{
-            //    mc.AddProfile(new MappingProfile());
-            //});
-
-            // IMapper mapper = mappingConfig.CreateMapper();
-            // services.AddSingleton(mapper);
-
-            //services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-
-            services.AddAutoMapper(typeof(MappingProfile));
-
-            services.AddIdentity<User, IdentityRole>(cfg =>
-            {
-                /*Reference:
-                 * https://docs.microsoft.com/en-us/aspnet/core/security/authentication/identity?view=aspnetcore-2.2&tabs=visual-studio
-                 */
-                cfg.User.RequireUniqueEmail = true;
-                cfg.Password.RequireDigit = false;
-                cfg.Password.RequiredUniqueChars = 0;
-                cfg.Password.RequireLowercase = false;
-                cfg.Password.RequireNonAlphanumeric = false;
-                cfg.Password.RequireUppercase = false;
-                cfg.Password.RequiredLength = 6;
-            })
-            .AddEntityFrameworkStores<DataContext>();
-
             services.AddDbContext<DataContext>(cfg =>
             {
-                //cfg.UseMySql(this.Configuration.GetConnectionString("MySQLConnection"));
-                cfg.UseSqlServer(this.Configuration.GetConnectionString("SQLServerConnection"));
+                cfg.UseMySql(this.Configuration.GetConnectionString("MySQLConnection"));
+                //cfg.UseSqlServer(this.Configuration.GetConnectionString("SQLServerConnection"));
             });
-
-            services.AddAuthentication().AddCookie().AddJwtBearer(cfg =>
-            {
-                cfg.TokenValidationParameters = new TokenValidationParameters
-                {
-                    ValidIssuer = this.Configuration["Tokens:Issuer"],
-                    ValidAudience = this.Configuration["Tokens:Audience"],
-                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(this.Configuration["Tokens:Key"]))
-                };
-            });
-
-            
-            services.AddTransient<Seeder>();
-
-
-
-            /*Startup*/
-            /*DI for Product*/
-            services.AddScoped<IOperations<Product>, ManageOperations<Product>>();
-            services.AddScoped<IRepository<Product>, GenericRepository<Product>>();
-            /*DI for Inventory*/
-            services.AddScoped<IOperations<Inventory>, ManageOperations<Inventory>>();
-            services.AddScoped<IRepository<Inventory>, GenericRepository<Inventory>>();
-            /*DI for Customer*/
-            services.AddScoped<IOperations<Customer>, ManageOperations<Customer>>();
-            services.AddScoped<IRepository<Customer>, GenericRepository<Customer>>();
-            /*DI for Supplier*/
-            services.AddScoped<IOperations<Supplier>, ManageOperations<Supplier>>();
-            services.AddScoped<IRepository<Supplier>, GenericRepository<Supplier>>();
-            /*DI for InventoryIO*/
-            services.AddScoped<IOperations<Inventoryio>, ManageOperations<Inventoryio>>();
-            services.AddScoped<IRepository<Inventoryio>, GenericRepository<Inventoryio>>();
-            /*DI for Purchase*/
-            services.AddScoped<IOperations<Purchaseorder>, ManageOperations<Purchaseorder>>();
-            services.AddScoped<IRepository<Purchaseorder>, GenericRepository<Purchaseorder>>();
-            /*DI for Order items sales*/
-            services.AddScoped<IOperations<Orderitemssales>, ManageOperations<Orderitemssales>>();
-            services.AddScoped<IRepository<Orderitemssales>, GenericRepository<Orderitemssales>>();
-            /*DI for Category*/
-            services.AddScoped<IOperations<Category>, ManageOperations<Category>>();
-            services.AddScoped<IRepository<Category>, GenericRepository<Category>>();
-
-
-            //services.AddScoped<IProductRepository, ProductRepository>();
-
-            services.AddScoped<IUserHelper, UserHelper>();
-
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
-            
-            services.ConfigureApplicationCookie(options =>
-            {
-                options.LoginPath = "/Account/NotAuthorized";
-                options.AccessDeniedPath = "/Account/NotAuthorized";
-            });
-
-            services.AddHttpContextAccessor();
-            //services.AddScoped<AlertService>();
-
-            //services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
-                .AddSessionStateTempDataProvider();
-            services.AddSession();
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -149,21 +46,18 @@ namespace App.Web
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
-            app.UseStatusCodePagesWithReExecute("/error/{0}");
-
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-            app.UseCookiePolicy();
-            app.UseAuthentication();
 
-            app.UseSession();
+            app.UseRouting();
 
-            app.UseMvc(routes =>
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
             {
-                routes.MapRoute(
+                endpoints.MapControllerRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
