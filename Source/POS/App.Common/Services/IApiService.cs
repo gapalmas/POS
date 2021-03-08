9923a88e44c4aa679a3a81e82ef9001a05aae482
Guillermo Alberto Palma Solano <gapalmas@outlook.com>
2021-03-08 11:41:50 -0700

    Add Response & ApiService Entities on Common.

---
+++ Source/POS/App.Common/Services/IApiService.cs
@@ -0,0 +1,13 @@
+﻿using App.Common.Responses;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Common.Services
+{
+    public interface IApiService
+    {
+        Task<Response> GetListAsync<T>(string urlBase, string servicePrefix, string controller);
+    }
+}
