d0b56e57b289358588003c0616b58e363929b993
Guillermo Alberto Palma Solano <gapalmas@outlook.com>
2021-03-08 11:24:35 -0700

    Add on Common Entities for ApiProducts | Add Nuget Packages (Xamarin.FFImageLoading.Forms” ) on App.Movil

---
+++ Source/POS/App.Common/Entities/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace App.Common.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        [DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode = false)]
+        public string ImagePath { get; set; }
+        public string PartNumber { get; set; }
+        public int CategoryId { get; set; }
+        public int ClasificationId { get; set; }
+        public int InventoryId { get; set; }
+    }
+}
--- Source/POS/App.Movil/App.Movil.iOS/AppDelegate.cs
+++ Source/POS/App.Movil/App.Movil.iOS/AppDelegate.cs
@@ -23,7 +23,7 @@ namespace App.Movil.iOS
         {
             global::Xamarin.Forms.Forms.Init();
             LoadApplication(new App(new iOSInitializer()));
-
+            FFImageLoading.Forms.Platform.CachedImageRenderer.Init();
             return base.FinishedLaunching(app, options);
         }
     }
